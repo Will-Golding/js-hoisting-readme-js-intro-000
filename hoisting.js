4ffb8a4ad3ae100eaf449854fef05f3cd69f83bb
will Golding <willgolding117@gmail.com>
2018-04-12 18:47:12 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -3,7 +3,7 @@ function callMe() {
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  
+
   var lyric = "maybe";
   return lyric;
 }
@@ -11,11 +11,12 @@ function callMe() {
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
+  
 
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+  thisIsCrazy();
 }
 
 function sayMyName() {
@@ -25,7 +26,8 @@ function sayMyName() {
   sayMy();
 
   function sayMy() {
-    console.log(name);
+    
     var name = "Kristin";
+    console.log(name);
   }
 }
